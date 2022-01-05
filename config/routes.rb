e12c95502491c090d616318ac47c4f977efc533a
TibreaA <andrei_tibrea2000@yahoo.com>
2022-01-06 01:40:27 +0200

    First steps for item

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,14 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  post 'items/search', to: 'items#search'
 
   resources :categorys
   resources :sub_categorys
+  resources :items
+
+
+
   get '*path', to: 'pages#index', via: :all
   root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
